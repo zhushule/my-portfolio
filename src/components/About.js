c649800701c371255d160e4857f23853155450f2
Shule Zhu <zhushule1996@gmail.com>
2024-10-21 12:01:01 -0400

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,11 @@ const About = () => {
       <div className="about-content">
         <h2>About Me</h2>
         <p>
-          Hello! I'm Shule Zhu, --------------------------------------------------------------------------------------------------------
-        </p>
-        <p>
-          -----------------------------------------------------------------------------------------
+          Hello! I'm Shule Zhu from South Carolina. I am currently employed as a Software Engineer at Capgemini. 
+          I focus on creating scalable, efficient, and innovative solutions that meet business needs.
+          Throughout my career, I have earned several certifications and successfully delivered multiple applications from concept to deployment.
+          My career goal is to continue growing in the software engineering field, with the aspiration of becoming a leader 
+          who guides and mentors teams in building high-impact technology solutions.
         </p>
       </div>
     </section>
